03ce43bbeb51fbdf769cdc6bf80a58b3da3bd4e5
vikas yadav <thejsdeveloper@gmail.com>
2024-01-18 11:42:38 +0530

    feat: adding menu section in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "@/components/AboutUs";
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
 
 export default function Home() {
   return (
     <main className="flex flex-col pt-20">
       <Hero />
       <AboutUs />
+      <Menu />
     </main>
   );
 }

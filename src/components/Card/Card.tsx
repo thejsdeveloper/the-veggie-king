e182a205f03730f2b59030fd8406378b87031009
vikas yadav <thejsdeveloper@gmail.com>
2024-01-27 14:46:28 +0530

    feat: responsive menu

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,16 +4,16 @@ import * as React from "react";
 
 function Card({ img, altText, title, description }: Card) {
   return (
-    <div className="w-96 cursor-pointer shadow  rounded-br-2xl rounded-bl-2xl bg-white">
+    <div className="group cursor-pointer shadow  rounded-br-2xl rounded-bl-2xl bg-white">
       <div className="overflow-hidden">
         <Image
           src={img}
           alt={altText}
-          className="hover:scale-125 ease-linear duration-500"
+          className="group-hover:scale-125 ease-linear duration-500"
         />
       </div>
-      <div className="py-6 px-5 space-y-4">
-        <h1 className="text-gray-900 text-md font-semibold tracking-wide">
+      <div className="py-6 px-5 space-y-2 lg:space-y-4">
+        <h1 className="text-gray-900 text-sm xl:text-md font-semibold tracking-wide">
           {title}
         </h1>
         <p className="text-gray-900 text-xs font-normal">{description}</p>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,19 +4,36 @@ import { MenuCards } from "@/constants";
 
 function Menu() {
   return (
-    <section className="flex flex-col py-32" id="menu">
-      <div className="flex-1 flex flex-col items-center justify-center space-y-4">
+    <section
+      className="flex flex-col 
+                  pt-32 
+                  px-4 md:px-12 lg:px-104 "
+      id="menu"
+    >
+      <div
+        className="flex-1 flex flex-col 
+                  items-center 
+                  justify-center space-y-4
+      "
+      >
         <h3 className="text-orange-600 text-sm font-normal tracking-wide">
           Menu
         </h3>
-        <h2 className="text-gray-900 text-lg font-semibold tracking-tighter">
+        <h2 className="text-gray-900 text-md md:text-lg font-semibold tracking-tighter">
           Explore our best food
         </h2>
-        <p className="text-gray-900 text-xs font-normal">
+        <p className="text-gray-900 text-xs md:text-sm font-normal text-center">
           Below you can see our best selling meals!
         </p>
       </div>
-      <div className="flex flex-row justify-center mt-8 px-104 flex flex-row space-x-8">
+      <div
+        className="flex 
+                  flex-col md:flex-row 
+                  items-center md:justify-center 
+                  mt-8
+                  space-y-8 md:space-y-0
+                  space-x-0 md:space-x-3 lg:space-x-8"
+      >
         {MenuCards.map((menu) => (
           <Card key={menu.id} {...menu} />
         ))}

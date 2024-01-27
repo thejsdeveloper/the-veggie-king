a295018f55f802b068a4b360dbd04a32a7f30a37
vikas yadav <thejsdeveloper@gmail.com>
2024-01-27 14:50:29 +0530

    feat: responsive gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -11,11 +11,11 @@ const photos = [photo1, photo2, photo3, photo4, photo5, photo6];
 
 function Gallery() {
   return (
-    <section className="flex flex-col">
-      <h2 className="flex flex-row justify-center mb-7 text-gray-900 text-lg font-semibold tracking-tighter">
+    <section className="flex flex-col pt-32">
+      <h2 className="flex flex-row justify-center mb-7 text-gray-900 text-md lg:text-lg font-semibold tracking-tighter">
         Good food = happy clients
       </h2>
-      <div className="grid grid-rows-2 grid-cols-3">
+      <div className="grid grid-rows-auto md:grid-rows-2 md:grid-cols-3">
         {photos.map((photo, i) => (
           <div
             className="hover:scale-95 ease-linear duration-500"

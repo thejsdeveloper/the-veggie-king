ff0d52a2956222b89c9cf679f01f43d46dcd2126
vikas yadav <thejsdeveloper@gmail.com>
2024-01-27 14:21:19 +0530

    feat: responsive about us

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -7,25 +7,51 @@ function AboutUs() {
   return (
     <section
       id="about-us"
-      className="grid grid-rows-1 grid-cols-2 pt-32  pr-104"
+      className="flex flex-col-reverse 
+      md:grid grid-rows-1 grid-cols-2
+      pl-4 md:pr-12 lg:pr-104 
+      pt-5 md:pt-32
+      overflow-hidden
+      "
     >
-      <div className="flex-1 h-646 relative">
-        <div className="flex flex-column items-center absolute -left-1/3 bg-green-600 h-646 w-646 rounded-full">
+      <div className="h-358 md:h-477 xl:h-646 relative">
+        <div
+          className="
+          absolute
+          -top-1/4 md:top-0
+          left-2/3 
+          md:-left-[80%]
+          lg:-left-[60%]
+          xl:-left-[30%]
+          flex flex-column items-center 
+          bg-green-600 
+          h-358 w-358
+          md:h-477 md:w-477
+          xl:h-646 xl:w-646 
+          rounded-full"
+        >
           <Image
             src={aboutUsFoodImage}
             alt="about us meal"
-            className="absolute left-1/2"
+            className="
+            absolute 
+            scale-75 xl:scale-100
+            -left-[30%] md:left-[45%]"
           />
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-center space-y-4 h-full">
+      <div
+        className="flex-1 flex flex-col justify-center 
+                  space-y-4 
+                  h-full w-[75%] md:w-full"
+      >
         <h3 className="text-orange-600 text-sm font-normal tracking-wide">
           About Us
         </h3>
-        <h2 className="text-gray-900 text-lg font-semibold tracking-tighter">
+        <h2 className="text-gray-900 text-md md:text-lg font-semibold tracking-tighter">
           Few words about us
         </h2>
-        <p className="text-gray-900 text-sm font-normal">
+        <p className="text-gray-900 text-xs md:text-sm font-normal">
           We are not ordinary food truck. We create a place overflowing with
           positive energy that it expresses important to us values. Get to know
           them specifically.
